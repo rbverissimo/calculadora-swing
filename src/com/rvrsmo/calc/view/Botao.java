37c02254ee2dc16ad52c01eac2046a234ef4ac2a
rbverissimo <rverissimo.dev@gmail.com>
2023-01-14 14:10:13 -0300

    Botao: definição de uma borda preta usando o BorderFactory.createLineBorder

--- src/com/rvrsmo/calc/view/Botao.java
+++ src/com/rvrsmo/calc/view/Botao.java
@@ -3,18 +3,21 @@ package com.rvrsmo.calc.view;
 import java.awt.Color;
 import java.awt.Font;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class Botao extends JButton{
 	
 	public Botao(String texto, Color cor) {
+		
 		setText(texto);
-		setOpaque(true);
-		setBackground(cor);
 		setFont(new Font("courier", Font.PLAIN, 22));
 		setForeground(Color.WHITE);
-		
+		setOpaque(true);
+		setBackground(cor);
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
 		
 	}
 

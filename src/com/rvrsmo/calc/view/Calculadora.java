d1998a425530f5b0f2069d93cd61ddc80bbcbd72
rbverissimo <rverissimo.dev@gmail.com>
2023-01-13 23:25:27 -0300

    criado o JFrame da Calculadora

---
+++ src/com/rvrsmo/calc/view/Calculadora.java
@@ -0,0 +1,20 @@
+package com.rvrsmo.calc.view;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Calculadora extends JFrame{
+	
+	public Calculadora() {
+		setSize(232, 322);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		
+	}
+	
+	public static void main(String[] args) {
+		new Calculadora();
+	}
+
+}

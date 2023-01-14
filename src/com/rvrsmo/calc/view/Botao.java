5e1d6bd4ca4e159761ae1a31a2b600665f71eeb8
rbverissimo <rverissimo.dev@gmail.com>
2023-01-14 13:51:20 -0300

    Botao: definição do construtor com definições visuais de texto, cor, fonte, tamanho de fonte, etc

--- src/com/rvrsmo/calc/view/Botao.java
+++ src/com/rvrsmo/calc/view/Botao.java
@@ -1,8 +1,20 @@
 package com.rvrsmo.calc.view;
 
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class Botao extends JButton{
+	
+	public Botao(String texto, Color cor) {
+		setText(texto);
+		setOpaque(true);
+		setBackground(cor);
+		setFont(new Font("courier", Font.PLAIN, 25));
+		
+		
+	}
 
 }

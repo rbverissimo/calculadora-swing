806631df0d18bfe022fe4c3d8afa5b54eec25ff8
rbverissimo <rverissimo.dev@gmail.com>
2023-01-14 13:47:12 -0300

    Display: atualização do Layout usando FlowLayout

--- src/com/rvrsmo/calc/view/Display.java
+++ src/com/rvrsmo/calc/view/Display.java
@@ -1,6 +1,7 @@
 package com.rvrsmo.calc.view;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
 
 import javax.swing.JLabel;
@@ -17,6 +18,9 @@ public class Display extends JPanel {
 		label = new JLabel("1234,56");
 		label.setForeground(Color.WHITE);
 		label.setFont(new Font("courier", Font.PLAIN, 30));
+		
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+		
 		add(label); 
 		
 		

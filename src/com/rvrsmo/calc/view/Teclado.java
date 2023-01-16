021caaf5b58a74473f522af386e8da0c8cf16446
rbverissimo <rverissimo.dev@gmail.com>
2023-01-15 22:05:56 -0300

    Classe Teclado: primeira versão do GridBagLayout para administrar o tamanho dos botões de forma mais flexível do que com o GridLayout

--- src/com/rvrsmo/calc/view/Teclado.java
+++ src/com/rvrsmo/calc/view/Teclado.java
@@ -1,6 +1,8 @@
 package com.rvrsmo.calc.view;
 
 import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JPanel;
@@ -13,11 +15,17 @@ public class Teclado extends JPanel {
 	private final Color COR_LARANJA = new Color(242, 163, 60); 
 	
 	public Teclado() {
-		setLayout(new GridLayout(5, 4));
-		add(new Botao("AC", COR_CINZA_ESCURO));
-		add(new Botao("+/-", COR_CINZA_ESCURO));
-		add(new Botao("%", COR_CINZA_ESCURO));
-		add(new Botao("/", COR_LARANJA));
+		
+		GridBagLayout layout = new GridBagLayout();
+		GridBagConstraints c = new GridBagConstraints();
+		
+		setLayout(layout);
+		
+		adicionarBotao("AC", COR_CINZA_ESCURO, c, 0, 0);
+		adicionarBotao("+/-", COR_CINZA_ESCURO, c, 1, 0);
+		adicionarBotao("%", COR_CINZA_ESCURO, c, 2, 0);
+		adicionarBotao("7", COR_LARANJA, c, 3, 0);
+		
 		
 		add(new Botao("7", COR_CINZA_CLARO));
 		add(new Botao("8", COR_CINZA_CLARO));
@@ -39,6 +47,16 @@ public class Teclado extends JPanel {
 		add(new Botao("9", COR_CINZA_CLARO));
 		add(new Botao("*", COR_LARANJA));
 		
+		
+	}
+
+	private void adicionarBotao(String texto, Color cor,
+			GridBagConstraints c, int x, int y) {
+		
+		c.gridx = x;
+		c.gridy = y; 
+		Botao botao = new Botao(texto, cor);
+		add(botao, c);
 		
 	}
 

6871d7cfcb4e5b88f9ea02f545af10744d5f714d
rbverissimo <rverissimo.dev@gmail.com>
2023-01-16 12:09:11 -0300

    Display: uso do método getTextoAtual no JLabel do Display

--- src/com/rvrsmo/calc/view/Display.java
+++ src/com/rvrsmo/calc/view/Display.java
@@ -7,6 +7,8 @@ import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import com.rvrsmo.calc.model.Memoria;
+
 @SuppressWarnings("serial")
 public class Display extends JPanel {
 	
@@ -15,7 +17,7 @@ public class Display extends JPanel {
 	public Display() {
 		
 		setBackground(new Color(46, 49, 50));
-		label = new JLabel("1234,56");
+		label = new JLabel(Memoria.getInstancia().getTextoAtual());
 		label.setForeground(Color.WHITE);
 		label.setFont(new Font("courier", Font.PLAIN, 30));
 		
